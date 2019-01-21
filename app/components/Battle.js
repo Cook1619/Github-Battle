a49f34fa7654f730c1429eadbb34e1952b2bf5e4
Cook1619 <cook6348@gmail.com>
2019-01-21 06:55:47 -0600

    Built out results component, and omdularized the playerpreview comp

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,42 +1,15 @@
 var React = require('react');
 var PropTypes = require('prop-types');
 var Link = require('react-router-dom').Link;
-
-function Playerpreview(props) {
-  return (
-    <div>
-      <div className="column">
-        <img
-          className="avatar"
-          src={props.avatar}
-          alt={'Avatar for ' + props.username}
-        />
-        <h2 className="username">@{props.username}</h2>
-      </div>
-      <button
-        className="reset"
-        onClick={props.onReset.bind(null, props.id)}
-      >
-        Reset
-      </button>
-    </div>
-  )
-}
-
-Playerpreview.propTypes = {
-  avatar: PropTypes.string.isRequired,
-  username: PropTypes.string.isRequired,
-  onReset: PropTypes.func.isRequired,
-  id: PropTypes.string.isRequired
-}
+var PlayerPreview = require('./PlayerPreview');
 
 class PlayerInput extends React.Component {
   constructor(props) {
-    super(props)
-
+    super(props);
     this.state = {
       username: ''
-    }
+    };
+
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -47,7 +20,7 @@ class PlayerInput extends React.Component {
       return {
         username: value
       }
-    })
+    });
   }
   handleSubmit(event) {
     event.preventDefault();
@@ -55,26 +28,26 @@ class PlayerInput extends React.Component {
     this.props.onSubmit(
       this.props.id,
       this.state.username
-    )
+    );
   }
   render() {
     return (
-      <form className="column" onSubmit={this.handleSubmit}>
-        <label className="header" htmlFor="username">
-          {this.props.label}
-        </label>
+      <form className='column' onSubmit={this.handleSubmit}>
+        <label className='header' htmlFor='username'>{this.props.label}</label>
         <input
-          id="username"
-          placeholder="github username"
-          type="text"
-          autoComplete='off'
+          id='username'
+          placeholder='github username'
+          type='text'
           value={this.state.username}
-          onChange={this.handleChange} />
-        <button className="button"
-          type="submit"
+          autoComplete='off'
+          onChange={this.handleChange}
+        />
+        <button
+          className='button'
+          type='submit'
           disabled={!this.state.username}>
-          Submit
-          </button>
+            Submit
+        </button>
       </form>
     )
   }
@@ -83,28 +56,30 @@ class PlayerInput extends React.Component {
 PlayerInput.propTypes = {
   id: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
-  onSubmit: PropTypes.func.isRequired
+  onSubmit: PropTypes.func.isRequired,
+}
+
+PlayerInput.defaultProps = {
+  label: 'Username',
 }
 
 class Battle extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
       playerOneName: '',
       playerTwoName: '',
       playerOneImage: null,
-      playerTwoImage: null
+      playerTwoImage: null,
+    };
 
-    }
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleReset = this.handleReset.bind(this);
   }
   handleSubmit(id, username) {
     this.setState(function () {
       var newState = {};
       newState[id + 'Name'] = username;
-      newState[id + 'Image'] = 'https://github.com/' + username + '.png?size=200';
+      newState[id + 'Image'] = 'https://github.com/' + username + '.png?size=200'
       return newState;
     });
   }
@@ -114,30 +89,35 @@ class Battle extends React.Component {
       newState[id + 'Name'] = '';
       newState[id + 'Image'] = null;
       return newState;
-    });
+    })
   }
   render() {
     var match = this.props.match;
     var playerOneName = this.state.playerOneName;
-    var playerTwoName = this.state.playerTwoName;
     var playerOneImage = this.state.playerOneImage;
+    var playerTwoName = this.state.playerTwoName;
     var playerTwoImage = this.state.playerTwoImage;
+
     return (
       <div>
-        <div className="row">
+        <div className='row'>
           {!playerOneName &&
             <PlayerInput
               id='playerOne'
               label='Player One'
               onSubmit={this.handleSubmit}
             />}
+
           {playerOneImage !== null &&
-            <Playerpreview
+            <PlayerPreview
               avatar={playerOneImage}
-              username={playerOneName}
-              onReset={this.handleReset}
-              id='playerOne'
-            />}
+              username={playerOneName}>
+                <button
+                  className='reset'
+                  onClick={this.handleReset.bind(this, 'playerOne')}>
+                    Reset
+                </button>
+            </PlayerPreview>}
 
           {!playerTwoName &&
             <PlayerInput
@@ -145,13 +125,17 @@ class Battle extends React.Component {
               label='Player Two'
               onSubmit={this.handleSubmit}
             />}
+
           {playerTwoImage !== null &&
-            <Playerpreview
+            <PlayerPreview
               avatar={playerTwoImage}
-              username={playerTwoName}
-              onReset={this.handleReset}
-              id='playerTwo'
-            />}
+              username={playerTwoName}>
+                <button
+                  className='reset'
+                  onClick={this.handleReset.bind(this, 'playerTwo')}>
+                    Reset
+                </button>
+            </PlayerPreview>}
         </div>
 
         {playerOneImage && playerTwoImage &&
@@ -161,7 +145,7 @@ class Battle extends React.Component {
               pathname: match.url + '/results',
               search: '?playerOneName=' + playerOneName + '&playerTwoName=' + playerTwoName
             }}>
-            Battle
+              Battle
           </Link>}
       </div>
     )
--- app/utils/api.js
+++ app/utils/api.js
@@ -42,7 +42,7 @@ function getUserData(player) {
 
     return {
       profile: profile,
-      score: calculateScore(profile, repos);
+      score: calculateScore(profile, repos)
     }
   })
 }
